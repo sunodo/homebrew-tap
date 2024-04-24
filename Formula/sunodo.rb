62b8c4e32e8288e55bd485451428ceffca1dfd7a
Danilo Tuler <tuler@pobox.com>
2024-04-24 14:08:13 -0400

    feat: bump version to 0.13.0

--- Formula/sunodo.rb
+++ Formula/sunodo.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Sunodo < Formula
   desc "Sunodo CLI"
   homepage "https://github.com/sunodo/sunodo"
-  url "https://registry.npmjs.org/@sunodo/cli/-/cli-0.12.0.tgz"
-  sha256 "3a93bd6064a4d2dc66d816d99755645a7489498e303511a19c7417a0c736fe43"
+  url "https://registry.npmjs.org/@sunodo/cli/-/cli-0.13.0.tgz"
+  sha256 "a81fdcca81072ff53edf40ee3fe50c0697759e9c148965e0491fbb77f7d61dc3"
   license "Apache-2.0"
 
   livecheck do
